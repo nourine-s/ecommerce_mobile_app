102dc77bb449147ef739ff0bd63ba255bc6cd722
Nourine <120821754+nourine-s@users.noreply.github.com>
2024-05-05 15:16:02 +0300

    cart_screen.dart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -13,7 +13,6 @@ class CartScreen extends StatefulWidget {
 }
 class _CartScreenState extends State<CartScreen> {
   final user = FirebaseAuth.instance.currentUser!;
-
   @override
   Widget build(BuildContext context) {
     return Consumer<Cart>(
