b66ebac9129c22d4cd8b72281669266b807da4d1
Nourine <120821754+nourine-s@users.noreply.github.com>
2024-05-05 15:16:45 +0300

    mydrawer.dart

--- lib/screens/mydrawer.dart
+++ lib/screens/mydrawer.dart
@@ -10,7 +10,7 @@ class MyDrawer extends StatelessWidget {
       Navigator.pop(context);
       Navigator.of(context).pushReplacementNamed('HomeScreen');
     }
-
+    
     void openProfileScreen() {
       Navigator.of(context).pushReplacementNamed('ProfileScreen');
     }
